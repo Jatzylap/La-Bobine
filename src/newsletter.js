3df9bbbbe15cc410382df269fee1ffe8f851a9c5
Jatzylap <j4tzplayz@gmail.com>
2026-07-14 16:10:56 +0200

    remove prisma

--- src/newsletter.js
+++ src/newsletter.js
@@ -1,15 +1,9 @@
-/*
-function getLink()
+function saveEmail()
 {
-  var link = document.getElementById('bouton-mail');
-  var sender = document.getElementById('bouton-form').value;
-  var subject = "Demande d'abonnement newsletter";
-  var message = "Bonjour, je voudrais suivre le newsletter de la Bobine! \nMon e-mail: " + sender;
-  var receiver = "yokoknighter@gmail.com";
-  var href = "mailto:" + receiver + "?subject=" + subject + "&body=" + message;
-  if (sender != "") link.setAttribute("href", href);
-  else link.setAttribute("href", "");
-}
-*/
+  document.getElementById('bouton-form').value = "";
+  document.getElementById('message').classList.toggle('toggle');
 
-do something like nodejs code here
+
+  
+  return false;
+}
