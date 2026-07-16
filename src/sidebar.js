7f5a1a99b8ee89fa316ef0e25dbc6b5b571dcc94
Jatzylap <j4tzplayz@gmail.com>
2026-07-16 20:49:30 +0200

    wacky buttons

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,5 +1,19 @@
 function clickMenu()
 {
     const sidebar = document.getElementById("sidebar");
+    const overlay = document.getElementById("overlay");
     sidebar.classList.toggle("open");
+    overlay.classList.toggle("open");
+    overlay.onclick = () => clickMenu();
+}
+
+function clickPrograms(event)
+{
+    if (event) {
+        event.preventDefault();
+    }
+
+    const sidebar = document.getElementById("sidebar");
+    sidebar.classList.toggle("programs-open");
+    return false;
 }
