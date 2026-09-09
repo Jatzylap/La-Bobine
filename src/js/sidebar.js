5815fa5db57e00838b83d9951f5064f281f3c27c
Jatzylap <j4tzplayz@gmail.com>
2026-09-09 18:31:12 +0200

    remove fetches

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -1,6 +1,10 @@
+import html from './sidebar.html';
+
+const sidebar = document.getElementById("sidebar");
+sidebar.innerHTML = html;
+
 function clickMenu()
 {
-    const sidebar = document.getElementById("sidebar");
     const overlay = document.getElementById("overlay");
     sidebar.classList.toggle("open");
     overlay.classList.toggle("open");
@@ -9,7 +13,24 @@ function clickMenu()
 
 function clickPrograms()
 {
-    const sidebar = document.getElementById("sidebar");
     sidebar.classList.toggle("programs-open");
     return false;
+}
+
+/*
+function saveEmail()
+{
+  document.getElementById('bouton-form').value = "";
+  document.getElementById('message').classList.add('toggle');
+  return false;
+}
+*/
+
+function sendEmail()
+{
+  var sender = 'cine.bobine.uzel@gmail.com';
+  var matiere = 'Demande de suggestion';
+  var saut = '%0A';
+  var contenu = "Bonjour,"+saut+saut+"Voici mes propositions pour la programmation du Ciné-Club La Bobine: "+saut+saut+"Merci et bonne saison à toute l'équipe.";
+  window.open('mailto:'+sender+'?subject='+matiere+'&body='+contenu, '_blank');
 }
